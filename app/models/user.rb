686c52d47f12e4da48ad59f403cbd89ab1056b56
driedjack@gmail.com <driedjack@gmail.com>
2019-08-30 17:13:59 +0700

    Validates users
    Signed-off-by: driedjack@gmail.com <driedjack@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,21 @@ class User < ApplicationRecord
          :confirmable
 
   validates :username, :gender, :birthday, presence: true
+  validates :username, length: { in: 2..25, allow_blank: true },
+                       format: { with: /\A[a-z][a-z0-9_\-\s]*\z/i, allow_blank: true }
+  validate :in_valid_birthday_range
 
   enum gender: %i[male female not_both]
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
+  end
+
+  private
+
+  def in_valid_birthday_range
+    unless ((Date.today - 90.years)..(Date.today-1.day)).cover? birthday
+      errors.add(:birthday, :not_in_range)
+    end
   end
 end
