7a5ca59c7c640a70348f1c2dd4f8c90d3a3e4dd6
driedjack <driedjack@gmail.com>
2019-09-04 22:46:24 +0700

    Use shouda matchers
    Signed-off-by: driedjack <driedjack@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ApplicationRecord
 
   validates :username, :gender, :birthday, presence: true
   validates :username, length: { in: 2..25, allow_blank: true },
-                       format: { with: /\A[a-z][a-z0-9_\-\s]*\z/i, allow_blank: true }
+                       format: { with: /\A[a-z][a-z0-9_\-\s]*\z/i,
+                                 allow_blank: true }
   validate :in_valid_birthday_range
 
   enum gender: %i[male female not_both]
@@ -22,8 +23,7 @@ class User < ApplicationRecord
   private
 
   def in_valid_birthday_range
-    unless ((Date.today - 90.years)..(Date.today-1.day)).cover? birthday
-      errors.add(:birthday, :not_in_range)
-    end
+    errors.add(:birthday, :not_in_range) unless
+      ((Date.today - 90.years)..(Date.today - 1.day)).cover? birthday
   end
 end
