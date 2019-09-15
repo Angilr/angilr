02c47fa87195e497ae4ccba9a0db2015b6bd74ec
driedjack@gmail.com <driedjack@gmail.com>
2019-09-15 21:05:50 +0700

    Secret gmail password
    Signed-off-by: driedjack@gmail.com <driedjack@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,10 +44,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            'angilr.develr@gmail.com',
-    password:             'ppgchkyfuwljivxj',
+    user_name:            Rails.application.credentials.gmail[:username],
+    password:             Rails.application.credentials.gmail[:password],
     authentication:       'plain',
-    enable_starttls_auto: true }
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
